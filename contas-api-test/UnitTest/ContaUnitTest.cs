f71b963e80dbdc1d4f87e80a71f4307df6966265
Filipe Pedro <filipe.pedroads@gmail.com>
2022-05-10 22:21:09 -0300

    Itens que ficaram faltando

--- contas-api-test/UnitTest/ContaUnitTest.cs
+++ contas-api-test/UnitTest/ContaUnitTest.cs
@@ -5,10 +5,6 @@ using contas_api_model.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace contas_api_test.UnitTest
@@ -30,17 +26,32 @@ namespace contas_api_test.UnitTest
         }
 
         [Fact]
-        public void Alterar_ValidaSeValorPagoNaoEZero_RetornaUmErro()
+        public void Salvar_ValidaSeQuantidadeDeParcelasEstaZerada_DeveRetornarUmErro()
         {
             //Arrange
-            Conta conta = new Conta {Id = 1, ValorTotal = 0, ValorRestante = 0, FormaPagamentoId = 1 };
+            Conta conta = new Conta {Id = 1, ValorTotal = 150, ValorRestante = 150, FormaPagamentoId = 2 };
 
             //Act
-            Action act = () => _contaRepository.VerifyIfCanUpdate(null);
+            Action act = () => _contaRepository.VerificaSeNumeroDeParcelasEstaZerado(conta);
 
             //Assert
             Exception exception = Assert.Throws<Exception>(act);
-            Assert.Equal("Erro ao alterar conta!", exception.Message);
+            Assert.Equal("Não é possível salvar uma conta parcelada com o número de parcelas zerado!", exception.Message);
+        }
+
+        [Fact]
+        public void Salvar_ValidaSeDataDeVencimentoEMaiorQueDataAtual_DeveRetornarUmErro()
+        {
+            //Arrange
+            DateTime dataDeValidade = DateTime.UtcNow.AddDays(5);
+            Conta conta = new Conta { Id = 1, ValorTotal = 150, ValorRestante = 150, DataValidade = dataDeValidade, FormaPagamentoId = 2 };
+
+            //Act
+            Action act = () => _contaRepository.VerificaDataDeValidade(conta);
+
+            //Assert
+            Exception exception = Assert.Throws<Exception>(act);
+            Assert.Equal("Não é possível salvar uma conta com a data de vencimento maior que a data de hoje!", exception.Message);
         }
     }
 }
--- contas-api/Startup.cs
+++ contas-api/Startup.cs
@@ -1,9 +1,12 @@
 using contas_api_model;
 using contas_api_model.Entity;
 using contas_api_model.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
 namespace contas_api
@@ -32,24 +35,24 @@ namespace contas_api
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        //public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        //{
-        //    if (env.IsDevelopment())
-        //    {
-        //        app.UseDeveloperExceptionPage();
-        //        app.UseSwagger();
-        //        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "contas_api v1"));
-        //    }
+        //This method gets called by the runtime.Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "contas_api v1"));
+            }
 
-        //    app.UseRouting();
+            app.UseRouting();
 
-        //    app.UseAuthorization();
+            app.UseAuthorization();
 
-        //    app.UseEndpoints(endpoints =>
-        //    {
-        //        endpoints.MapControllers();
-        //    });
-        //}
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
     }
 }
